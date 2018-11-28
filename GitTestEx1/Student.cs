fc4659cb0bb273c6872cebc8fa5856986ca87907
xodn4098 <xodn4794@gmail.com>
2018-11-28 12:02:42 +0900

    스튜던트 윈폼 생성, 폼 로드시 mbox 출력기능 추가

---
+++ GitTestEx1/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTestEx1
+{
+    public partial class Student : Form
+    {
+        public Student()
+        {
+            InitializeComponent();
+        }
+
+        private void Student_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+    }
+}
